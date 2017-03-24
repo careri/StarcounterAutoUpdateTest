5e2b390a1a59feb6472e9472349ba9b7322b566d
Carl Ericsson <Carl.Ericsson@heads.com>
2017-03-24 11:32:05 +0100

    Added client and server test

--- AutoUpdateTest/Program.cs
+++ AutoUpdateTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Starcounter;
+using System.Threading;
 
 namespace AutoUpdateTest
 {
@@ -10,17 +11,78 @@ namespace AutoUpdateTest
             Application.Current.Use(new HtmlFromJsonProvider());
             Application.Current.Use(new PartialToStandaloneHtmlProvider());
 
-            Handle.GET("/UpdateTest", () =>
+            Handle.GET("/UpdateTest/client", () =>
             {
-                var json = new UpdateTestJson();
+                var json = new UpdateTestJson
+                {
+                    IsAutoUpdate = true,
+                    Message = "Client auto update test"
+                };
 
                 if (Session.Current == null)
                 {
-                    Session.Current = new Session();
+                    Session.Current = new Session(SessionOptions.PatchVersioning);
                 }
                 json.Session = Session.Current;
                 return json;
             });
+
+            Handle.GET("/UpdateTest/server", () =>
+            {
+                var json = new UpdateTestJson
+                {
+                    IsAutoUpdate = false,
+                    Message = "Server auto update test"
+                };
+
+                if (Session.Current == null)
+                {
+                    Session.Current = new Session(SessionOptions.PatchVersioning);
+                }
+                json.Session = Session.Current;
+                TimeUpdater.Start(json, TimeSpan.FromSeconds(1));
+                return json;
+            });
+
+            
         }
+        private class TimeUpdater
+        {
+            private readonly Session m_session;
+            private readonly UpdateTestJson m_json;
+            private readonly TimeSpan m_interval;
+            private readonly Timer m_timer;
+
+            public TimeUpdater(Session session, UpdateTestJson json, TimeSpan interval)
+            {
+                m_session = session;
+                m_json = json;
+                m_interval = interval;
+                m_timer = new Timer(OnTimer, null, interval, Timeout.InfiniteTimeSpan);
+            }
+
+            private void OnTimer(object state)
+            {
+                if (m_session.IsAlive())
+                {
+                    m_session.CalculatePatchAndPushOnWebSocket();
+                }
+                else
+                {
+                    m_timer.Dispose();
+                }
+            }
+
+            internal static void Start(UpdateTestJson json, TimeSpan interval)
+            {
+                var session = json.Session;
+
+                if (session != null)
+                {
+                    new TimeUpdater(session, json, interval);
+                }
+            }
+        }
+
     }
 }
